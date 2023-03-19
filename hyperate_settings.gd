7ce23e01eacafdfb1ef3aa66a8eaffe9baafdf7c
Yannick Fricke <yannickfricke@googlemail.com>
2023-03-20 00:06:55 +0100

    feat: added the HypeRateSettings resource
    Now the developer has an easy way to change the endpoint URL, their API
    token and the interval when a phoenix heartbeat should be sent.

---
+++ hyperate_settings.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+@export
+var endpoint_url: String = "wss://app.hyperate.io/socket/websocket"
+
+@export
+var api_token: String
+
+@export_range(5, 30)
+var heartbeat_interval: int = 10
--- plugin.gd
+++ plugin.gd
@@ -3,14 +3,14 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	# Add the "HypeRate" custom node to the editor
-	# add_custom_type("HypeRate", "Node", preload("hyperate.gd"), preload("logo.png"))
+	add_custom_type("HypeRateSettings", "Resource", preload("hyperate_settings.gd"), preload("logo.png"))
 
 	# Let Godot only instantiate one instance of the socket
 	add_autoload_singleton("HypeRate", "res://addons/hyperate/hyperate.gd")
 
 func _exit_tree() -> void:
 	# Removes the "HypeRate" custom node
-	# remove_custom_type("HypeRate")
+	remove_custom_type("HypeRateSettings")
 
 	# Remove the singleton instance of the socket
 	remove_autoload_singleton("HypeRate")
