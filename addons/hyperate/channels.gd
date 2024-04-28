90df38f12ec766a3242e9d172b0c602a7241a73d
Yannick Fricke <yannickfricke@googlemail.com>
2024-04-28 16:19:24 +0200

    style(channels): formatted file

--- addons/hyperate/channels.gd
+++ addons/hyperate/channels.gd
@@ -3,21 +3,21 @@
 class_name hyperate_channels
 
 ## Contains all channels which should be joined
-var _joining_channels : Array[String] = []
+var _joining_channels: Array[String] = []
 
 ## Contains all channels where the socket already sent a "join channel" packet
 var _intermediate_joining_channels: Array[String] = []
 
 ## Contains all channels which where joined
-var _joined_channels  : Array[String] = []
+var _joined_channels: Array[String] = []
 
 ## Contains all channels which should be left
-var _leaving_channels : Array[String] = []
+var _leaving_channels: Array[String] = []
 
-var _intermediate_leaving_channels : Array[String] = []
+var _intermediate_leaving_channels: Array[String] = []
 
 ## Adds the given channel to the list of channels which should be joined
-func add_joining_channel(channel_name : String) -> bool:
+func add_joining_channel(channel_name: String) -> bool:
     if _joined_channels.has(channel_name):
         return false
 
@@ -29,7 +29,7 @@ func add_joining_channel(channel_name : String) -> bool:
 
 ## Removes the given channel from the list of channels which should be joined and
 ## adds it to the list of joined channels
-func add_joined_channel(channel_name : String) -> bool:
+func add_joined_channel(channel_name: String) -> bool:
     if _joined_channels.has(channel_name):
         return false
 
@@ -44,7 +44,7 @@ func add_joined_channel(channel_name : String) -> bool:
 ## Leaves the given channel
 ## Checks if the channel is joined - if not - false is returned
 ## ATTENTION: It will not check if the given channel_name is in the list of channels to join
-func leave_channel(channel_name : String) -> bool:
+func leave_channel(channel_name: String) -> bool:
     if _leaving_channels.has(channel_name):
         return false
 
@@ -61,7 +61,7 @@ func filter_channels(channels: Array[String], channel_to_remove: String) -> Arra
 func _to_string() -> String:
     return "Channels { joining: %s, joined: %s, leaving: %s }" % [_joining_channels, _joined_channels, _leaving_channels]
 
-func get_join_channel_packet(channel_name : String):
+func get_join_channel_packet(channel_name: String):
     return JSON.stringify({
         "topic": ("hr:%s" % channel_name),
         "event": "phx_join",
@@ -69,7 +69,7 @@ func get_join_channel_packet(channel_name : String):
         "ref": 0
         })
 
-func get_leave_channel_packet(channel_name : String):
+func get_leave_channel_packet(channel_name: String):
     return JSON.stringify({
         "topic": ("hr:%s" % channel_name),
         "event": "phx_leave",
